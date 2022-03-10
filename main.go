8e0d62f5c82975e47d6bab3a5b5b5457bc936a7a
Luke Massa <lmassa@tripadvisor.com>
2022-03-09 21:31:46 -0500

    Add args

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/lukemassa/landgrab/pkg/landgrab"
 )
 
 func main() {
-	landgrab.DetermineAttackers([]int{1})
+	attackers := make([]int, len(os.Args)-1)
+	for i := 0; i < len(os.Args)-1; i++ {
+		attacker, err := strconv.Atoi(os.Args[i+1])
+		if err != nil {
+			panic(err)
+		}
+		attackers[i] = attacker
+	}
+	landgrab.DetermineAttackers(attackers)
 }
