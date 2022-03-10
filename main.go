4a6793614d42f3d0e111c339cc61d30fc84c4615
Luke Massa <lmassa@tripadvisor.com>
2022-03-09 21:38:19 -0500

    Require at least one arg

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		panic("Must enter at least one territory")
+	}
 	attackers := make([]int, len(os.Args)-1)
 	for i := 0; i < len(os.Args)-1; i++ {
 		attacker, err := strconv.Atoi(os.Args[i+1])
