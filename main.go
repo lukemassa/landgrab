ffa5fd6d3d2630b3b6fa96bcd22afb0b1bf206fa
Luke Massa <lmassa@tripadvisor.com>
2022-04-13 13:24:15 -0400

    Comments

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	if len(os.Args) < 2 || strings.HasPrefix(os.Args[1], "-") {
 		usage()
 	}
+    // Convert args into slice of ints, panic if it can't parse
 	attackers := make([]int, len(os.Args)-1)
 	for i := 0; i < len(os.Args)-1; i++ {
 		attacker, err := strconv.Atoi(os.Args[i+1])
